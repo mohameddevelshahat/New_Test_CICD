eca02ea70aa9c8d3830623ede0d3a779cd94b2e4
Mohamed Elshahat <mohameddevelshahat@gmail.com>
2026-03-26 16:13:11 +0200

    Login Suite Commit

---
+++ Test Suites/Regression_Suite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Regression_Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>852c02d8-2ab9-42cf-8a9c-454d260faaae</testSuiteGuid>
+</TestSuiteEntity>
